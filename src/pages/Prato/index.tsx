3b29b7fd4835b67bab569532c664a122eab498bf
jdsn90 <jefferson.pessoal@hotmail.com>
2022-09-21 16:19:03 -0300

    ReactRoutes_Aula06_Ajustes finais

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,16 +2,18 @@ import styles from './Prato.module.scss';
 import { useParams, useNavigate } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
-  if(!prato) {
-    return '';
+  if (!prato) {
+    return <NotFound />;
   }
   return (
-    <>
+    <PaginaPadrao>
       <button className={styles.voltar} onClick={() => navigate(-1)}>
         {'< Voltar'}
       </button>
@@ -29,6 +31,6 @@ export default function Prato() {
         </div>
         <TagsPrato {...prato} />
       </div>
-    </>
+    </PaginaPadrao>
   );
 }
